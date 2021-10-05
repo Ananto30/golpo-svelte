eae43db2348d0fdb2bbe9e78abf1a52d1fe853d4
ananto30 <azizulhaq.ananto@gmail.com>
2021-10-05 11:53:22 +0600

    Add api client

---
+++ src/client.js
@@ -0,0 +1,112 @@
+import axios from "axios";
+import commonStore from "./store/commonStore";
+
+const api = axios.create({
+  baseURL: "http://locahost:8080/api",
+  // baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const getHeader = () => {
+  return {
+    headers: {
+      Authorization: "Bearer " + commonStore.authToken,
+    },
+  };
+};
+
+const errorHandler = (err) => {
+  if (
+    err.response &&
+    (err.response.status === 401 || err.response.status === 403)
+  ) {
+    commonStore.resetAuth();
+  }
+};
+
+// const verifySuccessCalls = (res) => {
+//   if (res.status === 200) return res;
+// };
+
+const Auth = {
+  login: (username, password) =>
+    api.post("/auth/login", {
+      username: username,
+      password: password,
+    }),
+  getGoogleAuthUrl: () => api.get("/auth/login/google/getAuthUrl"),
+  getTokenByGoogleCode: (code) =>
+    api.post("/auth/login/google/getToken", { code: code }),
+};
+
+const User = {
+  getAllUsers: () => api.get("/user", getHeader()).catch(errorHandler),
+  getByUsername: (username) =>
+    api.get(`/user/${username}`, getHeader()).catch(errorHandler),
+  updateMeta: (meta) =>
+    api.post("/user/me/update", meta, getHeader()).catch(errorHandler),
+
+  //TODO: this one is pretty tricky, need to rethink about the header design
+  getUsersMeta: (usernames) =>
+    api
+      .get("/user/usersmeta", {
+        params: { usernames: usernames },
+        headers: {
+          Authorization: "Bearer " + commonStore.authToken,
+        },
+      })
+      .catch(errorHandler),
+};
+
+const Post = {
+  getAll: () => api.get("/post", getHeader()).catch(errorHandler),
+  getById: (id) => api.get(`/post/${id}`, getHeader()).catch(errorHandler),
+  getByUsername: (username) =>
+    api.get(`/post/user/${username}`, getHeader()).catch(errorHandler),
+  createPost: (text) =>
+    api.post("/post", { text: text }, getHeader()).catch(errorHandler),
+  createComment: (postId, text) =>
+    api
+      .post(`/post/${postId}/comment`, { text: text }, getHeader())
+      .catch(errorHandler),
+};
+
+const Chat = {
+  getChats: () => api.get("/chat", getHeader()).catch(errorHandler),
+  getChatsByReceiver: (receiver) =>
+    api.get(`/chat/${receiver}`, getHeader()).catch(errorHandler),
+  sendChat: (receiver, text) =>
+    api
+      .post(
+        `/chat/${receiver}`,
+        {
+          text: text,
+        },
+        getHeader()
+      )
+      .catch(errorHandler),
+  sendMessage: (receiver, text) =>
+    api
+      .post(
+        `/chat/${receiver}/message`,
+        {
+          text: text,
+        },
+        getHeader()
+      )
+      .catch(errorHandler),
+};
+
+const Activity = {
+  getAll: () => api.get("/activity", getHeader()).catch(errorHandler),
+};
+
+export default {
+  Auth,
+  User,
+  Post,
+  Chat,
+  Activity,
+};
