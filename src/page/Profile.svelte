3d2807fc37bfae8260b24f8a57aba44f86dffe73
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2021-10-05 19:32:16 +0200

    Change profile data

--- src/page/Profile.svelte
+++ src/page/Profile.svelte
@@ -98,33 +98,10 @@
 			</div>
 			<div class="p-2">
 				<h3 class="text-center text-xl text-gray-900 font-medium leading-8">Joh Doe</h3>
-				<div class="text-center text-gray-400 text-xs font-semibold">
-					<p>Web Developer</p>
+				<div class="text-center text-gray-500 text-xs">
+					<p>Google Inc.</p>
+					<p><i>Work hard, play nice :)</i></p>
 				</div>
-				<table class="text-xs my-3">
-					<tbody
-						><tr>
-							<td class="px-2 py-2 text-gray-500 font-semibold">Address</td>
-							<td class="px-2 py-2">Chatakpur-3, Dhangadhi Kailali</td>
-						</tr>
-						<tr>
-							<td class="px-2 py-2 text-gray-500 font-semibold">Phone</td>
-							<td class="px-2 py-2">+977 9955221114</td>
-						</tr>
-						<tr>
-							<td class="px-2 py-2 text-gray-500 font-semibold">Email</td>
-							<td class="px-2 py-2">john@exmaple.com</td>
-						</tr>
-						<tr>
-							<td class="px-2 py-2 text-gray-500 font-semibold">Work/Education</td>
-							<td class="px-2 py-2">Google Inc.</td>
-						</tr>
-						<tr>
-							<td class="px-2 py-2 text-gray-500 font-semibold">Tagline</td>
-							<td class="px-2 py-2">Work hard, play nice :)</td>
-						</tr>
-					</tbody>
-				</table>
 
 				<div class="text-center my-3">
 					<a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#"
