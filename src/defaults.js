7ec6e3c4b7377e6f5ebdc93477a45ecd656a1574
ananto30 <azizulhaq.ananto@gmail.com>
2021-10-22 23:41:50 +0600

    Move base url to defaults

--- src/defaults.js
+++ src/defaults.js
@@ -2,3 +2,4 @@ export const IMAGE_LARGE = "/images/charlie2.jpeg";
 export const IMAGE_SMALL = "/images/avatar/small/daniel.jpg";
 export const WORK = "Workless";
 export const TAGLINE = "This one has no tagline!";
+export const BASE_URL = "http://localhost:8080/api";
