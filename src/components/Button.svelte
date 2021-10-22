ca3b6aa3a94f15b98fa54e1c32cf87146fbffa9b
Meet Gor <40317114+Mr-Destructive@users.noreply.github.com>
2021-10-22 17:42:16 +0530

    Created button.svelte for ripple effect
    Created a button svelte component with a ripple effect.

---
+++ src/components/Button.svelte
@@ -0,0 +1,32 @@
+<button class="ripple">
+	<span>Click me!</span>
+</button>
+
+
+<style>
+.ripple {
+  background-position: center;
+  transition: background 0.8s;
+}
+.ripple:hover {
+  background: #47a7f5 radial-gradient(circle, transparent 1%, #47a7f5 1%) center/15000%;
+}
+.ripple:active {
+  background-color: #ffffff;
+  background-size: 100%;
+  transition: background 0s;
+}
+
+button {
+  border: none;
+  border-radius: 2px;
+  padding: 12px 18px;
+  font-size: 16px;
+  text-transform: uppercase;
+  cursor: pointer;
+  color: white;
+  background-color: #2196f3;
+  box-shadow: 0 0 4px #999;
+  outline: none;
+}
+</style>
