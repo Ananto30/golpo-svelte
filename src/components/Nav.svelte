12e7db482b0b96f8c14d1c4942a5fa31d2decd9f
ananto30 <azizulhaq.ananto@gmail.com>
2021-11-05 16:55:15 +0600

    Add comment delete and has notification functionality

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,11 +1,21 @@
+<style>
+  .navshadow {
+    box-shadow: 0 4px 3px -3px rgba(184, 184, 184, 0.644);
+  }
+</style>
+
 <script>
   import { loggedUsername, jwt, page } from "../store.js";
+  import client from "../client";
+  import { onMount } from "svelte";
 
   import { fade } from "svelte/transition";
 
   import Svg from "../components/Svg.svelte";
 
   let sideBarVisible = true;
+  let hasNotification = false;
+  let shouldBorder = false;
   let navItems = [
     {
       name: "Home",
@@ -50,25 +60,54 @@
   const toggleSideBar = () => {
     sideBarVisible = !sideBarVisible;
   };
+
+  const hasNotificationCall = async () => {
+    const res = await client.Notification.hasUnclickedNotification();
+    if (res.data.hasUnclickedNotification) return true;
+    return false;
+  };
+
+  const navBorderTracker = () => {
+    const top = document.documentElement.scrollTop || document.body.scrollTop;
+    if (top != 0) {
+      shouldBorder = true;
+    } else {
+      shouldBorder = false;
+    }
+  };
+
+  onMount(async () => {
+    window.addEventListener("scroll", navBorderTracker);
+    hasNotification = await hasNotificationCall();
+  });
 </script>
 
 <div in:fade class="sticky top-0 z-20 w-full md:w-auto md:min-h-screen ">
   <!-- mobile menu bar -->
-  <div class="fixed z-50 flex justify-between w-full bg-white border-gray-300 md:w-auto md:hidden">
+  <div
+    class="{shouldBorder
+      ? 'border-b-2'
+      : ''} fixed z-50 flex justify-between w-full bg-white md:w-auto md:hidden"
+  >
     <!-- logo -->
     <a href="#/home" class="block px-4 py-2 my-auto text-2xl font-bold font-montserrat">Golpo</a>
 
     <!-- mobile menu button -->
     <button on:click="{toggleSideBar}" class="p-4 focus:outline-none active:bg-gray-100">
       <div class="flex my-auto">
-        <span class="relative inline-block ">
+       
           <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
-          <span
-            class="absolute top-0 right-0 inline-block w-2 h-2 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full animate-pulse"
-          ></span>
-        </span>
+          {#if hasNotification}
+          <span class="relative inline-block h-2">
+            <span class="flex w-2 h-2">
+              <span class="absolute inline-flex w-full h-full bg-black rounded-full opacity-75 animate-ping"></span>
+              <span class="relative inline-flex w-2 h-2 bg-black rounded-full"></span>
+            </span>
+          </span>
+          {/if}
+      
       </div>
     </button>
   </div>
@@ -93,16 +132,16 @@
             class="{$page == item.name.toLowerCase() ? 'font-bold text-yellow' : ''} {navButtonClass}"
             href="{item.href}"
           >
-            <div class="{item.name == 'Notification' ? 'animate-wiggle' : ''}">
+            <div class="{item.name == 'Notification' && hasNotification ? 'animate-wiggle' : ''}">
               <Svg name="{item.icon}" height="24" width="24" />
             </div>
-
             {item.name}
-            {#if item.name == "Notification"}
-              <span class="relative inline-block h-2">
-                <span
-                  class="absolute top-0 right-0 inline-block w-2 h-2 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full"
-                ></span>
+            {#if item.name == "Notification" && hasNotification}
+              <span class="relative inline-block -mt-4 -ml-2">
+                <span class="flex w-2 h-2">
+                  <span class="absolute inline-flex w-full h-full bg-black rounded-full opacity-75 animate-ping"></span>
+                  <span class="relative inline-flex w-2 h-2 bg-black rounded-full"></span>
+                </span>
               </span>
             {/if}
           </a>
@@ -131,9 +170,3 @@
     </div>
   </div>
 </div>
-
-<style>
-  .navshadow {
-    box-shadow: 0 4px 3px -3px rgba(184, 184, 184, 0.644);
-  }
-</style>
