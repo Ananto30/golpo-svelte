523f88b2f34ba51f212da8aafc5f82e55666e1b4
Azizul Haque Ananto <azizulhaq.ananto@gmail.com>
2025-02-15 22:17:25 +0100

    Add test workflow

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+test('home page loads', async ({ page }) => {
+    await page.goto('/');
+    await expect(page).toHaveTitle(/Golpo/);
+    await expect(page.locator('h1')).toBeVisible();
 });
