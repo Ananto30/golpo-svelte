236a34c835ead237d3a15cad4836528b4c2edf44
Azizul Haque Ananto <azizulhaq.ananto@gmail.com>
2025-02-16 21:24:32 +0100

    Fix default

--- src/defaults.js
+++ src/defaults.js
@@ -2,5 +2,5 @@ export const IMAGE_LARGE = '/images/charlie2.jpeg';
 export const IMAGE_SMALL = '/images/avatar/small/daniel.jpg';
 export const WORK = 'Workless';
 export const TAGLINE = 'This one has no tagline!';
-// export const BASE_URL = 'https://golpo-express.vercel.app/api';
-export const BASE_URL = 'http://localhost:8080/api';
+export const BASE_URL = 'https://golpo-express.vercel.app/api';
+// export const BASE_URL = 'http://localhost:8080/api';
